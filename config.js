f5627d45074d963057fbaeafb2bfd72ec2edd3b1
johnstark00000000-a11y <johnstark00000000@gmail.com>
2026-08-25 19:50:35 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAmG-m4ztMtLIzISJG6JY7Vs1pt7v1kKaM",
+  authDomain: "my-cpa-reward-app.firebaseapp.com",
+  projectId: "my-cpa-reward-app",
+  storageBucket: "my-cpa-reward-app.firebasestorage.app",
+  messagingSenderId: "769066727383",
+  appId: "1:769066727383:web:bd52d0668c84019cb4bd92",
+  measurementId: "G-G6L8SR8VJP"
+};
